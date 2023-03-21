bfc6bdc4896af60e66b2fcbe8812ec623a21d7b5
Orion Ballamis <oriondragon10@gmail.com>
2023-03-21 14:05:12 -0400

    initial commit

---
+++ maxoftwo.js
@@ -0,0 +1,14 @@
+console.log('hello world');
+
+let x = 1;
+let y = 2;
+
+if(x > y){
+    console.log("the greater value is", x)
+} else if(x === y) 
+{
+    console.log("the values are the same")
+}
+else {
+    console.log("the greater value is", y)
+}
